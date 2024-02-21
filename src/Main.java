ffee7660af11b1c6d0cce8388959ad3f22195956
Leyre <elvitxu@hotmail.com>
2024-02-21 13:18:29 +0100

    primer commit seguridad commit hasta alarma

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         // Press Alt+Intro with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-        System.out.printf("Hello and welcome!");
+
 
 
     }
